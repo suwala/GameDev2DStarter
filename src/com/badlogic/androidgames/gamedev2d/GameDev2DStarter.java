de7a117af586750898d5c6047f4161cae26c8bc2
unknown <double.accel.kk@gmail.com>
2012-12-11 15:53:44 +0900

    farst commit

---
+++ src/com/badlogic/androidgames/gamedev2d/GameDev2DStarter.java
@@ -0,0 +1,36 @@
+package com.badlogic.androidgames.gamedev2d;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class GameDev2DStarter extends ListActivity{
+	
+	String tests[] = {"CannonScreenTest"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tests));
+        
+    }
+    
+    @Override
+    protected void onListItemClick(ListView list,View view,int position,long id){
+    	
+    	super.onListItemClick(list, view, position, id);
+    	
+    	String testName = tests[position];
+    	try{
+    		Class clazz = Class.forName("com.badlogic.androidgames.gamedev2d."+testName);
+    		Intent intent = new Intent(this,clazz);
+    		startActivity(intent);
+    	}catch (ClassNotFoundException e){
+    		e.printStackTrace();
+    	}    	
+    }
+
+}
